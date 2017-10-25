ea89435eee45acd1318e91357e9f123cc4b32ec5
lucaswantz <lucaswantz@hotmail.com>
2017-10-25 15:22:49 -0200

    algumas alterações

--- Projeto Sistemas Operacionais/mysimpletop.c
+++ Projeto Sistemas Operacionais/mysimpletop.c
@@ -1,56 +1,51 @@
-//////////////////////////////////////////////////////////////////////
-//			        Alessandro Antoniolli		                            //
-//			        Monica Chiesa				                                //
-//			        Thainan Fagundes Brum	                	            //
-//			        Vitor Vian				                                  //
-//////////////////////////////////////////////////////////////////////
-
 #include <pthread.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-// Biblioteca que contém constante HZ (número de jiffies por segundo).
 #include <sys/param.h>
 
-float obterUsoDaCPUPeloProcesso(int pid, int tempoAtualizacao)
-{
+float obterUsoDaCPUPeloProcesso(int pid, int tempoAtualizacao) {
+	
     int vNULLI;
     char vNULLC[100];
     char nomeArquivo[100];
-
+	
     int pid_stat;
-    int jiffiesUsuario;
-    int jiffiesKernel;
-    float jiffiesTotal1;
-    float jiffiesTotal2;
-    float porcentagem;
-    FILE* stat;
+	
+    int timeUsuario;
+    int timeKernel;
+    float timeTotal1;
+    float timeTotal2;
+    float percent_usage;
+	
+    FILE* arquivo_stat;
 
+	// Grava string com nome do arquivo stat do processo selecionado
     sprintf(nomeArquivo, "/proc/%d/stat", pid);
 
     // Abre aquivo /proc/PID/stat.
-    if ( (stat = fopen(nomeArquivo, "r")) != NULL ){
-        fscanf(stat, "%d\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d", &pid_stat, vNULLC, vNULLC, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &jiffiesUsuario, &jiffiesKernel);
-        fclose(stat);
+    if ((arquivo_stat = fopen(nomeArquivo, "r")) != NULL ){
+        fscanf(arquivo_stat, "%d\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d", &pid_stat, vNULLC, vNULLC, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &timeUsuario, &timeKernel);
+        fclose(arquivo_stat);
 
-        // Soma total de jiffies.
-        jiffiesTotal1 = (float)(jiffiesUsuario + jiffiesKernel);
+        // Soma total de time.
+        timeTotal1 = (float)(timeUsuario + timeKernel);
     }
     sleep(tempoAtualizacao);
 
     // Abre aquivo /proc/PID/stat.
-    if ( (stat = fopen(nomeArquivo, "r")) != NULL ){
-        fscanf(stat, "%d\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d", &vNULLI, vNULLC, vNULLC, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &jiffiesUsuario, &jiffiesKernel);
-        fclose(stat);
+    if ( (arquivo_stat = fopen(nomeArquivo, "r")) != NULL ){
+        fscanf(arquivo_stat, "%d\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d", &vNULLI, vNULLC, vNULLC, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &vNULLI, &timeUsuario, &timeKernel);
+        fclose(arquivo_stat);
 
-        // Soma total de jiffies.
-        jiffiesTotal2 = (float)(jiffiesUsuario + jiffiesKernel);
+        // Soma total de time.
+        timeTotal2 = (float)(timeUsuario + timeKernel);
     }
-    porcentagem = (( (jiffiesTotal2 - jiffiesTotal1) / (HZ * tempoAtualizacao) ) * 100.00);
+	
+    percent_usage = (( (timeTotal2 - timeTotal1) / (HZ * tempoAtualizacao) ) * 100.00);
 
-    return porcentagem;
+    return percent_usage;
 }
 
 int obtemMemoriaTotalProcesso(int pid)
@@ -61,12 +56,11 @@ int obtemMemoriaTotalProcesso(int pid)
     int numeroDePaginasMemoria;
     int memoriaEmKb = 0;
 
-    // Obtém o nome do arquivo.
+    // Obtém o nome do arquivo stat.
     sprintf(nomeArquivo, "/proc/%d/statm", pid);
 
     // Abre o arquivo statm do processo.
-    if ( (statm = fopen(nomeArquivo, "r")) != NULL )
-    {
+    if ((statm = fopen(nomeArquivo, "r")) != NULL ) {
         fscanf(statm, "%d\t%d\t%d", &vNULLI, &numeroDePaginasMemoria, &vNULLI);
 
         fclose(statm);
@@ -74,6 +68,7 @@ int obtemMemoriaTotalProcesso(int pid)
         // Obtém o uso de memória em Kbytes.
         memoriaEmKb = numeroDePaginasMemoria * (getpagesize() / 1024);
     }
+	
     // obter a memoria
     memoriaEmKb = (memoriaEmKb * 100) / GetRamInKB();
 
@@ -104,9 +99,8 @@ int main (int argc, char *argv[])
     int tempo_atualizacao = 2;
     int i = 0;
 
-    printf("------------------------------------\n");
     printf("ID |   PID   |  Mémoria  |   CPU    |\n");
-    printf("------------------------------------\n");
+    printf(" \n");
 
     pid = atoi(argv[1]);
 
